fa599e5b901bf5ae253a2785fc777cdf1f9995ba
Shane Upchurch <shaneupchurch@gmail.com>
2021-03-03 13:31:09 -0600

    sets up file structures, components, services, etc.

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const header = props => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+header.propTypes = {
+
+};
+
+export default header;
---
+++ src/components/player/Spotify.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Spotify = props => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+Spotify.propTypes = {
+
+};
+
+export default Spotify;

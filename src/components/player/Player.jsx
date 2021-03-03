43df902def4f2658c1b95f685851802f468fe5b9
Franco Ortega <francopdx@gmail.com>
2021-03-03 15:05:13 -0800

    Feature/geolocation (#2)
    * add initial button to allow geolocation tracking
    
    * add getCurrentPosition method and success method to onTrackingClick
    
    * add coordinates object to hold tracking data
    
    * add error method for getCurrentPosition
    
    * add loading
    
    * add comments and clean up code

--- src/components/player/Player.jsx
+++ src/components/player/Player.jsx
@@ -1,10 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import Loading from '../loading/Loading';
 
 const Player = props => {
+  const [loading, setLoading] = useState(false);
+
+  const coordinates = {
+    latitude: '',
+    longitude: ''
+  };
+
+  const onTrackingClick = () => {
+    console.log('Tracking button clicked');
+
+    setLoading(true);
+
+    // success method passed into getCurrentPosition
+    // gets coordinates
+    // sets coordinates
+    const success = (position) => {
+      const lat = position.coords.latitude;
+      const long = position.coords.longitude;
+
+      coordinates.latitude = lat;
+      coordinates.longitude = long;
+
+      console.group(['User coordinates']);
+      console.log(coordinates);
+      console.groupEnd();
+
+      setLoading(false);
+    };
+
+    // error method passed into getCurrentPosition
+    const error = (err) => {
+      // error message displays in console if user denies access
+      console.warn(`Error(${err.code}): ${err.message}`);
+      setLoading(false);
+    };
+
+    // getCurrentPosition gets user location
+    navigator.geolocation.getCurrentPosition(success, error);
+  };
+
+  if(loading) return <Loading />;
+
   return (
     <div>
-      Player
+      <p>
+        <button onClick={onTrackingClick}>Enable Tracking</button>
+      </p>
+      <p>
+        Player
+      </p>
     </div>
   );
 };

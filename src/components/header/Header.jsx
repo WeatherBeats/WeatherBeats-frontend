83cd6b8e9bcf6939c18d86097b67e0894e2fc225
Shane Upchurch <shaneupchurch@gmail.com>
2021-03-04 17:35:11 -0600

    adds tiny bit of styling

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import About from '../about/About';
 import NavDropDown from '../menu/NavDropDown';
+import styles from './Header.css';
 
 const header = props => {
   return (
     <>
-    Header
+      <div className={styles.title}>
+        <h1>WeatherBeats</h1>
+      </div>
+      
       <NavDropDown />
     </>
   );

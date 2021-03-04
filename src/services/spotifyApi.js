a047f5b8ceccc9ab221d5d9704df3b3d9b3dcd09
Perry Sittser <sittserp@gmail.com>
2021-03-04 13:24:29 -0800

    spotify GET request (#6)

--- src/services/spotifyApi.js
+++ src/services/spotifyApi.js
@@ -1,2 +1,17 @@
 
-hello;
+export const getPlaylist = (searchTerm, authToken) => {
+  return fetch(
+    `https://api.spotify.com/v1/search?q=${searchTerm}&type=playlist`, 
+    {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${authToken}`,
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(res => res.json())
+    .then(json => json.playlist.items)
+    .then(items => items.map(item => (
+      item.id
+    )));
+};

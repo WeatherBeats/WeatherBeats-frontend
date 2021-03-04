6940536759e833b472a59ff83e1d79cae0f82c1b
Franco Ortega <francopdx@gmail.com>
2021-03-04 11:10:07 -0800

    add initial general layout styling for entire component

--- src/containers/home/Home.jsx
+++ src/containers/home/Home.jsx
@@ -2,10 +2,16 @@ import React from 'react';
 import styles from './Home.css';
 
 const Home = () => {
+
+  const onSpotifyLoginClick = () => {
+    console.log('Spotify login button clicked.');
+
+  };
+
   return (
     <div className={styles.Home}>
       Home
-      <button>Spotify</button>
+      <button onClick={onSpotifyLoginClick}>Spotify</button>
     </div>
   );
 };

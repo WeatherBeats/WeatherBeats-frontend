036ac9c77971d1e1da5ac338cb63d2b3325b14fb
Franco Ortega <francopdx@gmail.com>
2021-03-04 11:02:32 -0800

    add initial button and button styling

--- src/containers/home/Home.jsx
+++ src/containers/home/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import styles from './Home.css';
 
 const Home = () => {
   return (
-    <div>
+    <div className={styles.Home}>
       Home
+      <button>Spotify</button>
     </div>
   );
 };

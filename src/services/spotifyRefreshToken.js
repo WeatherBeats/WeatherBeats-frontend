0933b5a6a653bef95335161acd533e99e8ab09ce
Shane Upchurch <shaneupchurch@gmail.com>
2021-03-05 14:44:14 -0600

    cleaning up code

--- src/services/spotifyRefreshToken.js
+++ src/services/spotifyRefreshToken.js
@@ -2,12 +2,5 @@
 export const getNewAccessToken = (refreshToken) => {
   return fetch(
     `${process.env.API_URL}/refresh_token?refresh_token=${refreshToken}`)
-    // {
-    //   method: 'GET'
-    //   // headers: {
-    //   //   'Authorization': `Bearer ${authToken}`,
-    //   //   'Content-Type': 'application/json'
-    //   // }
-    // })
     .then(res => res.json());
 };

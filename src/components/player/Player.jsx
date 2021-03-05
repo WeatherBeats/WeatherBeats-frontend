14b8e2cee30cc682139e5e6b7e6fe8334ea3525c
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2021-03-05 14:58:33 -0500

    refactor and make newUserPlaylist function

--- src/components/player/Player.jsx
+++ src/components/player/Player.jsx
@@ -11,13 +11,19 @@ const Player = ({ match }) => {
   const [playlists, setPlaylists] = useState([]);
   const [userPlaylist, setUserPlaylist] = useState('');
 
+  const newUserPlaylist = (playlistIds) => {
+    const id = playlistIds[Math.floor(Math.random() * playlistIds.length)];
+    console.log('id', id);
+    // setUserPlaylist(id);
+    return id;
+  };
+
   const coordinates = {
     latitude: '',
     longitude: ''
   };
 
   const onTrackingClick = () => {
-
     setLoading(true);
 
     const success = (position) => {
@@ -30,15 +36,14 @@ const Player = ({ match }) => {
       postLocation(coordinates)
         .then(genre => {
           getPlaylist(genre, token)
-            .then(res => setPlaylists(res));
-
-          const playlist = playlists[Math.floor(Math.random() * playlists.length)];
-
-          setUserPlaylist(playlist);
+            .then(res => {
+              setPlaylists(res);
+              const id = newUserPlaylist(res);
+              setUserPlaylist(id);
+            });
 
           setLoading(false);
         });
-
     };
 
     const error = (err) => {
@@ -49,24 +54,32 @@ const Player = ({ match }) => {
     navigator.geolocation.getCurrentPosition(success, error);
   };
 
+  const onNextClick = () => {
+    newUserPlaylist(playlists);
+  };
+
   if(loading) return <Loading />;
 
   return (
     <div>
       <p>
-        <button onClick={onTrackingClick}>Enable Tracking</button>
+        <button onClick={onTrackingClick}>Generate Playlist</button>
       </p>
 
       { !userPlaylist 
-        ? <p>Please click &apos;Enable Tracking&apos; to find a playlist based on your weather!</p> 
+        ? <p>Please click &apos;Generate Playlist&apos; to find a weather-appropriate playlist based on your current location!</p> 
         :
-        <iframe
-          src={`https://open.spotify.com/embed/playlist/${userPlaylist}`}
-          width="300"
-          height="380"
-          frameBorder="0"
-          allowtransparency="true"
-          allow="encrypted-media"></iframe>
+        <div>
+          <iframe
+            src={`https://open.spotify.com/embed/playlist/${userPlaylist}`}
+            width="300"
+            height="380"
+            frameBorder="0"
+            allowtransparency="true"
+            allow="encrypted-media"></iframe>
+
+          <button onClick={onNextClick}>Next Playlist</button>
+        </div>
       }
     </div>
   );

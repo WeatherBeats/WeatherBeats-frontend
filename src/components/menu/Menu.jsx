c4ca159bcae36c1aaef4f00ca35aa8479df6b062
Franco Ortega <francopdx@gmail.com>
2021-03-08 15:46:01 -0800

    update dropdown menu from hover to click

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Menu.css';
 
 const Menu = () => {
+  const [menu, setMenu] = useState(false);
+
   return (
     <>
-      <div className={ styles.DropDown }>
-      
+      <div 
+        className={ `${styles.DropDown} ${menu && styles.active}` }
+        onClick={() => setMenu(menu => !menu)}
+      >
+
         <img src="/images/menu.png" alt="menu" />
-          
-      
-        <div className={ styles.DropContent }>
+
+        <div className={styles.DropContent}>
           <span><Link to="/">Home</Link></span>
           <span><Link to="/player">Player</Link></span>
           <span><Link to="/about">About Us</Link></span>

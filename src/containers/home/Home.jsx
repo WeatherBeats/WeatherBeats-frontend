0b6a586d5feff248ceca30bdf46c7a67ccbcbda9
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2021-03-09 17:02:42 -0500

    need to commit so i can pull

--- src/containers/home/Home.jsx
+++ src/containers/home/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
   return (
     <div className={styles.Home}>
       <p>
-        WeatherBeats generates a playlist based on the weather in your area.
+        <i>weather-inspired playlists based on the weather in your area</i>
       </p>
       {/* Logo Criteria: To comply with our licensing agreements, you must always attribute content from Spotify with the logo. In partner integrations, you should always use our full logo (icon + wordmark). We do allow using only our icon if it’s featured as an app icon on the app screen of a device. */}
       {/* Color Criteria: The Spotify green logo should only be used on a black or white background, for any other background you should use a monochrome logo.

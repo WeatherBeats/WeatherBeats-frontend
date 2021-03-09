10b06a3abc44da33378f0a7f76f892adcbd29e7a
Bryana Kitchen <67290473+bryanakitchen@users.noreply.github.com>
2021-03-09 14:32:02 -0500

    Feature/faq details (#31)
    * remove console logs
    
    * add cursors
    
    * add FAQs
    
    * update arrow
    
    * add a font
    
    * a little more styling

--- src/components/instructions/faqData.js
+++ src/components/instructions/faqData.js
@@ -1,14 +1,39 @@
+/* eslint-disable max-len */
 export const faqData = [
   {
-    question: 'Faqs',
-    response: 'hi'
+    question: 'What is WeatherBeats?',
+    response: 'WeatherBeats generates playlists based on the current weather in your area.'
   },
   {
-    question: 'go',
-    response: 'oh hello'
+    question: 'What does ‘Generate Playlist’ do?',
+    response: '‘Generate Playlist’ will check your current location as well as the weather of that location and provide you with weather-appropriate playlists to enjoy.'
   },
   {
-    question: 'here',
-    response: 'fake data'
+    question: 'What if I don’t want to give you my current location?',
+    response: 'Living off the grid? Just pop in a zip code of your choosing instead! (Country is optional.)'
+  },
+  {
+    question: 'QUESTION ABOUT GEOLOCATION ISSUE',
+    response: 'Include step-by-step instructions for Mac and Windows users.'
+  },
+  {
+    question: 'My weather has changed!',
+    response: 'No problem! If the weather has changed since you first began listening to a playlist, simply click ‘Check Weather Again’ to allow us to update your playlist to the latest weather.'
+  },
+  {
+    question: 'I don’t like this playlist.',
+    response: 'We understand your taste in music is unique, so we created some options for you.\nOne option is to click ‘Next Playlist’ to grab another playlist for the same weather.\nAnother option is to use our genre filter to find playlists that match the genre of your choosing.'
+  },
+  {
+    question: 'What if I want to listen to ‘sunny weather’ music on a gloomy day?',
+    response: 'We got you covered! Select your own weather in the Weather drop down to generate a playlist based on your desired type of weather.'
+  },
+  {
+    question: 'The ‘Next Playlist button is not showing up.',
+    response: 'This could mean one of two things. If you navigated away from the page and came back, we ask that you Check Weather Again to get the freshest weather results for your area. Or this could simply mean that the selected weather and/or genre is very specific and there are currently very few playlists on Spotify.'
+  },
+  {
+    question: 'I love this app!',
+    response: 'We’re so glad you’re enjoying WeatherBeats!.'
   },
 ];

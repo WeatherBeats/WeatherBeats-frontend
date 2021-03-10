e810a4fee02a6f646c9c5bbf93c8fefe68011ee6
Franco Ortega <francopdx@gmail.com>
2021-03-10 13:38:16 -0800

    move profile pics to public folder; update profile pic paths in aboutData.js

--- src/components/about/aboutData.js
+++ src/components/about/aboutData.js
@@ -2,35 +2,35 @@
 export const aboutData = [
   {
     name: 'Bryana Kitchen',
-    photo: '../src/assets/profilePics/Bryana.png',
+    photo: '/profilePics/Bryana.png',
     gitHub: 'https://github.com/bryanakitchen/',
     linkedIn: 'https://www.linkedin.com/in/bryanakitchen/',
     bio: 'Born and raised in the Mitten, Bryana enjoys spending time outside in nature or at a live music event.  She prides herself in tackling new projects and hobbies such as fluid painting and roller derby.'
   },
   {
     name: 'Donny Vu',
-    photo: '../src/assets/profilePics/Donny.png',
+    photo: '/profilePics/Donny.png',
     gitHub: 'https://github.com/DonnyLVu/',
     linkedIn: 'https://www.linkedin.com/in/donnylvu/',
     bio: 'Donny bio here. Placeholder words words words words words words words words words words words words words words words words words words words words.'
   },
   {
     name: 'Franco Ortega',
-    photo: '../src/assets/profilePics/Franco.jpg',
+    photo: '/profilePics/Franco.jpg',
     gitHub: 'https://github.com/franco-ortega/',
     linkedIn: 'https://www.linkedin.com/in/franco-ortega/',
     bio: 'Franco is a longtime Portlander who enjoys biking around the city, climbing artificial rock walls, playing boardgames, and fantasy cartography.'
   },
   {
     name: 'Perry Sittser',
-    photo: '../src/assets/profilePics/Perry.jpg',
+    photo: '/profilePics/Perry.jpg',
     gitHub: 'https://github.com/sittserp/',
     linkedIn: 'https://www.linkedin.com/in/sittserp/',
     bio: 'Perry bio here. Placeholder words words words words words words words words words words words words words words words words words words words words.'
   },
   {
     name: 'Shane Upchurch',
-    photo: '../src/assets/profilePics/Shane.png',
+    photo: '/profilePics/Shane.png',
     gitHub: 'https://github.com/ShaneUP1/',
     linkedIn: 'https://www.linkedin.com/in/shaneupchurch/',
     bio: 'Whether it\'s Kendrick Lamar on a road-trip, Pretty Lights in the gym, or Manu Chao in the kitchen, Shane is a lover of good tunes. When not behind the computer you can find him on the trail or at the beach with his family.'

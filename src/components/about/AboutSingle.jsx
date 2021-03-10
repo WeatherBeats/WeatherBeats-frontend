7b8d7ed3b1d8741125d25b8ff05f465ccfea3505
Franco Ortega <francopdx@gmail.com>
2021-03-09 16:39:06 -0800

    Fix/style about (#37)
    * move icon styling from AboutSingle.jsx to AboutSingle.css
    
    * add styling to make each AboutSingle container the same size regardless of the amount of text in the bio
    
    * add final styling for About page on desktop view
    
    * add final styling on About page for tablet view
    
    * clean up code

--- src/components/about/AboutSingle.jsx
+++ src/components/about/AboutSingle.jsx
@@ -5,37 +5,29 @@ import gitHubIcon from '../../assets/GitHub_icon.png';
 import linkedInIcon from '../../assets/LinkedIn_icon.png';
 
 const AboutSingle = ({ name, photo, gitHub, linkedIn, bio }) => {
-
-  const iconStyle = {
-    width: '25%', 
-    height: '25%',
-    paddingLeft: '5%',
-    paddingRight: '5%'
-  };
-
   return (
     <div className={styles.AboutSingle}>
-      <h2>{name}</h2>
-      <p><img 
-        src={photo} 
-        alt={name}
-      /></p>
-      <span>
-        {bio}
-      </span>
-      <p>
+      <p className={styles.topContainer}>
+        <h2>{name}</h2>
+        <img 
+          src={photo} 
+          alt={name}
+        />
+        <section className={styles.bio}>
+          {bio}
+        </section>
+      </p>
+      <p className={styles.iconContainer}>
         <a href={gitHub} target="_blank" rel="noreferrer">
           <img
             src={gitHubIcon}
             alt="GitHub icon"
-            style={iconStyle}
           />
         </a>
         <a href={linkedIn} target="_blank" rel="noreferrer">
           <img
             src={linkedInIcon}
             alt="LinkedIn icon"
-            style={iconStyle}
           />
         </a>
       </p>

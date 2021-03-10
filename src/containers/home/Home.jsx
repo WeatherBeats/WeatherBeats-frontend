0e98fb3f0bb44da9088c2cdaeb9b1b140b76ed39
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2021-03-10 18:22:22 -0500

    home page

--- src/containers/home/Home.jsx
+++ src/containers/home/Home.jsx
@@ -7,21 +7,10 @@ const Home = () => {
 
   return (
     <div className={styles.Home}>
-      {/* <p>
-        <i>weather-inspired playlists based on the weather in your area</i>
-      </p> */}
-      {/* Logo Criteria: To comply with our licensing agreements, you must always attribute content from Spotify with the logo. In partner integrations, you should always use our full logo (icon + wordmark). We do allow using only our icon if it’s featured as an app icon on the app screen of a device. */}
-      {/* Color Criteria: The Spotify green logo should only be used on a black or white background, for any other background you should use a monochrome logo.
-      The black logo should be used on light colored backgrounds.
-      The white logo should be used on dark colored backgrounds. */}
       <div className={styles.line}>
-        <p>
-            WeatherBeats
-        </p>
+        <p>WeatherBeats</p>
         <div>
-          <p>
-            Log in with
-          </p>
+          <p>Log in with</p>
           <a href="https://weatherbeats-staging.herokuapp.com/login">
             <img src={spotify} alt="Spotify logo"/>
           </a>

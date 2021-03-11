a653a85c42140368b0bca8ab039cdfbf374d49d8
Franco Ortega <francopdx@gmail.com>
2021-03-10 16:11:38 -0800

    add questions to address geolocation access

--- src/components/instructions/faqData.js
+++ src/components/instructions/faqData.js
@@ -9,12 +9,16 @@ export const faqData = [
     response: '‘Generate Playlist’ will check your current location as well as the weather of that location and provide you with weather-appropriate playlists to enjoy.'
   },
   {
+    question: '‘Generate Playlist’ isn\'t working?',
+    response: 'It may be that the location setting on your browser has been turned off. To reset this setting in Chrome, follow these steps: 1) Go to the Settings page. 2) Scroll down to Privacy and Security. 3) Go to Site Settings. 4) Scroll down to the Permissions section and click on Location. 5) Click on the https://weatherbeats.netlify.app link. 6) Set the Location to Allow. To reset this setting in Firefox, follow these steps: 1) Click on the lock icon to the left of the URL bar. 2) Scroll down the Permissions and under Permissions, click the ‘X’ next to ‘Blocked Temporarily’.'
+  },
+  {
     question: 'What if I don’t want to give you my current location?',
     response: 'Living off the grid? Just pop in a zip code of your choosing instead! (Country is optional.)'
   },
   {
-    question: 'QUESTION ABOUT GEOLOCATION ISSUE',
-    response: 'Include step-by-step instructions for Mac and Windows users.'
+    question: 'I told the browser to block access to my location, but now I want to allow access.',
+    response: 'The Location setting needs to be reset in the brower. If you are using Chrome, follow these steps: 1) Go to the Settings page. 2) Scroll down to Privacy and Security. 3) Go to Site Settings. 4) Scroll down to the Permissions section and click on Location. 5) Click on the https://weatherbeats.netlify.app link. 6) Set the Location to Allow. To reset this setting in Firefox, follow these steps: 1) Click on the lock icon to the left of the URL bar. 2) Scroll down the Permissions and under Permissions, click the ‘X’ next to ‘Blocked Temporarily’.'
   },
   {
     question: 'My weather has changed!',
